ecda56b3b103ab4275202272e7f6753937f2ee70
Shivansh2203 <168472217+Shivansh2203@users.noreply.github.com>
2025-04-06 11:32:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./Components/Home"
 function App() {
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4'>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-cyan-200 py-8 px-4'>
       <div className="text-center mb-8">
         <h1 className="text-5xl mb-4 text-gray-500 font-bold">AI <span className="hover:text-orange-500">Image</span> Enhancer {" "}</h1>
         <p className="text-lg text-gray-500">Upload Your Image</p>
